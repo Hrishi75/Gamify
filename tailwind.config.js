b2b690660f6f4b5dd1ec8c63a4d3319672da8162
Hrishikesh Borkar <hrishikeshborkar94@gmail.com>
2025-05-08 12:02:18 +0530

    added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zentry: ['zentry', 'sanf-serif'],
+        general: ['general', 'sanf-serif'],
+        'circular-web': ['circular-web', 'sanf-serif'],
+        'robert-medium': ['reobert-medium', 'sanf-serif'],
+        'robert-regular': ['robert-regular', 'sans-serif'],
+      },
+      colors:{
+        blue:{
+          50: '#DFDFF0',
+          75: '#DFDFF2',
+          100: '#F0F2FA',
+          200: '#010101',
+          300: '#4FB7DD',
+        },
+        voilet: {
+           300: '#5723FF'
+        },
+        yellow:{
+          100: '#8E983F',
+          300: '#EDFF66',
+        },
+      }
+    },
   },
   plugins: [],
 }
