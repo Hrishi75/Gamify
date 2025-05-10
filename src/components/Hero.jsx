99c91cd14ab03948586edfadc71ac4e6cfb4ac15
Hrishikesh Borkar <hrishikeshborkar94@gmail.com>
2025-05-10 12:36:05 +0530

    made improvements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,6 +16,7 @@ const Hero = () => {
 
     const totalVideos = 4;
     const nextVideoRef = useRef(null);
+    const currentVideoRef = useRef(null);
 
     const handleVideoLoaded = () => {
         setLoadedVideos((prev) => prev +1);
@@ -100,7 +101,7 @@ const Hero = () => {
             <div>
                <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
                 <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
-                    <video  ref={nextVideoRef} src={getVideoSrc(upcomingVideoIndex)} loop muted id='current-video' className='size-64 origin-center scale-150 object-cover object-center ' onLoadedData={handleVideoLoaded} />
+                    <video  ref={currentVideoRef} src={getVideoSrc(upcomingVideoIndex)} loop muted id='current-video' className='size-64 origin-center scale-150 object-cover object-center ' onLoadedData={handleVideoLoaded} />
                 </div>
 
                </div>
